12766b6a62fd52962cb73cd5a7822f17a46c3d7d
jesus- <guerrero.jesussdavid@gmail.com>
2024-06-02 17:45:58 -0500

    commit 02

--- administrador/inicioAdmin.php
+++ administrador/inicioAdmin.php
@@ -4,11 +4,11 @@
                 
                 <div class="jumbotron">
                     <h1 class="display-3">Bienvenido  <?php echo $nombreUsuario; ?> </h1>
-                    <p class="lead">Administra los subsidios del sitio web</p>
+                    <p class="lead">Administra los productos del sitio web</p>
                     <hr class="my-2">
                     <p>Administrar...</p>
                     <p class="lead">
-                        <a class="btn btn-primary btn-lg" href="seccion/gestionSub.php" role="button">Administrar Subsidios</a>
+                        <a class="btn btn-primary btn-lg" href="seccion/gestionSub.php" role="button">Administrar Productos</a>
                     </p>
                 </div>
 
